6f49a02c04590053830985b4399a053313d60b64
matheo050108 <98183442+matheo050108@users.noreply.github.com>
2022-07-27 15:16:04 +0200

    exoliferp-statut

---
+++ LISEZ MOI.md
@@ -0,0 +1,26 @@
+# Discord bot pour la Family Développement
+
+## Prévisualisation
+
+<img src="https://kasper-rasmussen.dk/assets/images/discord_bot/1.png" />
+<img src="https://kasper-rasmussen.dk/assets/images/discord_bot/2.png" />
+
+## Configurations
+
+#### Ouvrir le fichier: `configs.json`
+
+Activity type: `PLAYING`, `WATCHING`, `STREAMING` or `LISTENING`
+
+Token: [Créér un bot](https://discordapp.com/developers/applications/), copiez votre TOKEN et copiez le ici.
+
+Server info: Remplacer l'ip et le port par défaut par les votres..
+
+## Nécessaire
+
+[Install NodeJS](https://nodejs.org/en/download/)
+
+## Discord: https://discord.gg/tNR5cWV
+
+## Commandes : npm install discord.js   ##  npm i fivereborn-query 
+
+<a href="https://discord.gg/NHsABpb"><img src="https://kasper-rasmussen.dk/assets/images/icons/discord_logo.png" /></a>
---
+++ index.js
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////
+// VARIABLES
+/////////////////////////////////////////////////////
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const configs = require("./configs.json");
+const fivereborn = require('fivereborn-query');
+client.config = configs;
+
+/////////////////////////////////////////////////////
+// FONCTION (A NE PAS MODIFIER)
+/////////////////////////////////////////////////////
+
+function activity() {
+  setTimeout(() => {
+    fivereborn.query(configs.serverInfo[0], configs.serverInfo[1], (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        client.user.setActivity(" online " + data.clients + "/" + data.maxclients, { type: configs.activityType });
+      }
+    });
+    activity();
+  }, 10000);
+}
+activity();
+
+client.login(process.env.TOKEN);
